7eb22b99a90f8e08204f3fcdd219cf9d0bb5df99
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 17:17:33 +0530

    Added Framer Motion animations to the Navbar component.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import "./Navbar.scss"
+import {motion} from 'framer-motion'
+
 
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* Sidebar */}
       <div className="wrapper">
-        <span>Kirttinath</span>
+        <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Kirttinath</motion.span>
         <div className="social">
             <a href="#"><img src="/facebook.png" alt="" /></a>
             <a href="#"><img src="/instagram.png" alt="" /></a>
