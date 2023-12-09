8c2d8dc84e2e5327604a1b608fc8e324bc0c271a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 17:33:25 +0530

    Add toggle button and links to the sidebar.

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
 import "./Sidebar.scss"
-
+import ToogleButton from './ToogleButton/ToogleButton';
+import Links from './Links/Links';
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="bg">
-        Links
+        
       </div>
     </div>
   )
