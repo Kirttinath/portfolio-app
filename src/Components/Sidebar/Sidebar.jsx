6e3a715d50eb6f0563739fb50748ce9c23cb1772
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 18:43:08 +0530

    Update sidebar animation variants

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -10,14 +10,22 @@ const Sidebar = () => {
   const [open, setOpen] = useState(false);
 
   const variants = {
-    open: 
-    closed: 
+    open: {},
+    closed: {
+      clipPath: "circle(30px at 50px 50px)",
+      transition:{
+        delay:0.5,
+        type:"spring",
+        stiffness: 400,
+        damping: 40,
+      }
+    }
   }
   return (
-    <motion.div className="sidebar">
-      <div className="bg">
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
         <Links/>
-      </div>
+      </motion.div>
       <ToogleButton setOpen={setOpen}/>
     </motion.div>
   )
