b85b26405608a9f7b6a5dfbd746269ddbc5bfc3e
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 17:23:46 +0530

    Add Sidebar component to the Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import "./Navbar.scss"
 import {motion} from 'framer-motion'
-
+import Sidebar from '../Sidebar/Sidebar';
 
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* Sidebar */}
+      <Sidebar/>
       <div className="wrapper">
         <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Kirttinath</motion.span>
         <div className="social">
