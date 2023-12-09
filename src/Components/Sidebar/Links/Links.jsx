b68c10572aa0a268a9af3462d3d0049ef5b46aa0
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 19:23:55 +0530

    Update navigation links and add a hamburger menu.

--- src/Components/Sidebar/Links/Links.jsx
+++ src/Components/Sidebar/Links/Links.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+const items = [
+    "Homepage",
+    "Services",
+    "Portfolio",
+    "Contact",
+    "About",
+]
+
 const Links = () => {
   return (
-    <div className='links'>Links</div>
+    <div className='links'>{items.map(item => (
+      <a href={`#${item}`} key={item}>{item}</a>
+    ))}</div>
   )
 }
 
--- src/Components/Sidebar/ToogleButton/ToogleButton.jsx
+++ src/Components/Sidebar/ToogleButton/ToogleButton.jsx
@@ -1,8 +1,14 @@
-
+import { motion } from 'framer-motion';
 
 const ToogleButton = ({setOpen}) => {
   return (
-    <button onClick={() => setOpen(prev=>!prev)}>Button</button>
+    <button onClick={() => setOpen(prev=>!prev)}>
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round"/>
+        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round"/>
+        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round"/>
+      </svg>
+    </button>
   )
 }
 
