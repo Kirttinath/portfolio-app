751b2dff4b1d0cb21edcfbd341141ed61e4d89d8
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 20:39:21 +0530

    Update Navbar, Sidebar, Links, and ToggleButton components

--- src/Components/Sidebar/Links/Links.jsx
+++ src/Components/Sidebar/Links/Links.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import { motion } from 'framer-motion';
 
-const items = [
+const variants ={
+  open:{
+    transition:{
+      staggerChildren:0.1,
+    },
+  },
+  closed:{
+    transition:{
+      staggerChildren:0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants ={
+  open:{
+    y:0,
+    opacity: 1,
+  },
+  closed:{
+    y: 50,
+    opacity: 0,
+  },
+};
+
+
+const Links = () => {
+  const items = [
     "Homepage",
     "Services",
     "Portfolio",
     "Contact",
     "About",
-]
+];
 
-const Links = () => {
+  
   return (
-    <div className='links'>{items.map(item => (
-      <a href={`#${item}`} key={item}>{item}</a>
-    ))}</div>
+    <motion.div className='links' variants={variants}>
+      {items.map(item => (
+      <motion.a href={`#${item}`} key={item} variants={itemVariants}>{item}</motion.a>
+    ))}</motion.div>
   )
 }
 
