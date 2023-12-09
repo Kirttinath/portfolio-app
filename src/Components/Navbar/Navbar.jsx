460863aefbe713cc917e85791928c217f15f30f7
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 16:22:11 +0530

    Add social media icons to the navbar.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,16 @@ import "./Navbar.scss"
 const Navbar = () => {
   return (
     <div className="navbar">
-      
+      {/* Sidebar */}
+      <div className="wrapper">
+        <span>Kirttinath</span>
+        <div className="social">
+            <a href="#"><img src="/facebook.png" alt="" /></a>
+            <a href="#"><img src="/instagram.png" alt="" /></a>
+            <a href="#"><img src="/youtube.png" alt="" /></a>
+            <a href="#"><img src="/dribbble.png" alt="" /></a>
+        </div>
+      </div>
     </div>
   )
 }
