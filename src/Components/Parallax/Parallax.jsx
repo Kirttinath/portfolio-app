50c939d484b9999381ff14e885d301dc2a8c28fe
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 16:50:18 +0530

    Change background color of the parallax component based on the type prop.

--- src/Components/Parallax/Parallax.jsx
+++ src/Components/Parallax/Parallax.jsx
@@ -1,14 +1,21 @@
 import "./Parallax.scss";
 
-const Parallax = ({type}) => {
-  return (
-    <div className="parallax">
-      <h1>{type==="services"? "What I do?" : "What I did?"}</h1>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="stars"></div>
-    </div>
-  )
+const Parallax = ({ type }) => {
+    return (
+        <div className="parallax"
+            style={{
+                background:
+                    type === "services"
+                        ? "linear-gradient(180deg, #111132, #0c0c1d)"
+                        : "linear-gradient(180deg, #111132, #505064)"
+            }}
+        >
+            <h1>{type === "services" ? "What I do?" : "What I did?"}</h1>
+            <div className="mountains"></div>
+            <div className="planets"></div>
+            <div className="stars"></div>
+        </div>
+    )
 }
 
 export default Parallax
