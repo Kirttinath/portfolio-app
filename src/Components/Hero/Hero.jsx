f2aa2ea16625ae70b51c4eeb7d8405aa72d3dd7d
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 12:57:09 +0530

    font family changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,9 +3,13 @@ import './Hero.scss';
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="textContainer">
+        <h2>Kirttinath Ojha</h2>
+        <h1>Web Developer & UI Designer</h1>
+      </div>
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
-        </div>`
+        </div>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       {/* Sidebar */}
       <Sidebar/>
       <div className="wrapper">
-        <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Kirttinath</motion.span>
+        <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>KOjha</motion.span>
         <div className="social">
             <a href="#"><img src="/facebook.png" alt="" /></a>
             <a href="#"><img src="/instagram.png" alt="" /></a>
