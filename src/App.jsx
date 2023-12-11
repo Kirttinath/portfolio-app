0aef7fa97b5048953d7bdde8f3299b103841a1fc
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 16:35:37 +0530

    Added Parallax component to Services and Portfolio sections.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
+import Parallax from './Components/Parallax/Parallax';
 import './app.scss';
 
 
@@ -9,9 +10,9 @@ const App = () => {
       <Navbar />
        <Hero/>
     </section>
-    <section id='Services'>Parallax</section>
+    <section id='Services'><Parallax type="services"/></section>
     <section>Services</section>
-    <section id='Portfolio'>Parallax</section>
+    <section id='Portfolio'><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
