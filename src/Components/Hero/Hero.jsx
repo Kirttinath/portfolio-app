9b32883d962d9b05c4decac1d29e93659e4d2db7
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 15:06:51 +0530

    Update Hero component styling and layout

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,18 +3,21 @@ import './Hero.scss';
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="textContainer">
-        <h2>Kirttinath Ojha</h2>
-        <h1>Web Developer & UI Designer</h1>
-        <div className="buttons">
-          <button>See the latest Works</button>
-          <button>Contact Me</button>
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>Kirttinath Ojha</h2>
+          <h1>Web Developer & UI Designer</h1>
+          <div className="buttons">
+            <button>See the latest Works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="" />
         </div>
-        <img src="/scroll.png" alt="" />
       </div>
+
       <div className="imageContainer">
-        <img src="/hero.png" alt="" />
-        </div>
+        <img src="/me.png" alt="" />
+      </div>
     </div>
   )
 }
