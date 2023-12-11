e373dbdaf0b33ef699620f892e011353c22e99d3
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 19:28:13 +0530

    Update social media links in the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
       <div className="wrapper">
         <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>KOjha</motion.span>
         <div className="social">
+            <a href="#"><img src="/linkedin.png" alt="" /></a>
+            <a href="#"><img src="/github.png" alt="" /></a>
             <a href="#"><img src="/facebook.png" alt="" /></a>
             <a href="#"><img src="/instagram.png" alt="" /></a>
-            <a href="#"><img src="/youtube.png" alt="" /></a>
-            <a href="#"><img src="/dribbble.png" alt="" /></a>
         </div>
       </div>
     </div>
