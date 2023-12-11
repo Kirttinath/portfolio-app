c475f2c95ff0cf22b4ebb9e77ff2f19abeadcd9f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 13:10:52 +0530

    Add buttons and scroll image to Hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,6 +6,11 @@ const Hero = () => {
       <div className="textContainer">
         <h2>Kirttinath Ojha</h2>
         <h1>Web Developer & UI Designer</h1>
+        <div className="buttons">
+          <button>See the latest Works</button>
+          <button>Contact Me</button>
+        </div>
+        <img src="/scroll.png" alt="" />
       </div>
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
