b92b47610ac574406d697a83c1a6eef52b26440c
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 23:40:19 +0530

    Add Framer Motion to Parallax component

--- src/Components/Parallax/Parallax.jsx
+++ src/Components/Parallax/Parallax.jsx
@@ -1,19 +1,26 @@
+import { useRef } from "react";
 import "./Parallax.scss";
-import {motion} from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Parallax = ({ type }) => {
+    const ref = useRef();
+    const {scrollYProgress} = useScroll({
+        target:ref,
+        offset:["start start", "end start"],
+    });
+    const yBg = useTransform(scrollYProgress, [0,1],["0%","100%"]);
+    
     return (
-        <div className="parallax"
+        <div className="parallax" 
             style={{
                 background:
                     type === "services"
                         ? "linear-gradient(180deg, #111132, #0c0c1d)"
                         : "linear-gradient(180deg, #111132, #505064)"
-            }}
+            }} ref={ref}
         >
-            <motion.h1>{type === "services" ? "What I do?" : "What I did?"}</motion.h1>
-            <motion.div className="mountains">
-            </motion.div>
+            <motion.h1 style={{y: yBg}}>{type === "services" ? "What I do?" : "What I did?"}</motion.h1>
+            <motion.div className="mountains"></motion.div>
             <motion.div className="planets"></motion.div>
             <motion.div className="stars"></motion.div>
         </div>
