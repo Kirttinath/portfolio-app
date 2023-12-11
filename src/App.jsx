0094b8af981d6fcb5dd0c56f68bcb6b04afda443
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 20:44:38 +0530

    basic changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import './app.scss';
 
 
 const App = () => {
-  return <div>
-    <section id='Homepage'>
-      <Navbar />
-       <Hero/>
-    </section>
-    <section id='Services'><Parallax type="services"/></section>
-    <section>Services</section>
-    <section id='Portfolio'><Parallax type="portfolio"/></section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section id='Contact'>Contact</section>
-    </div>;
+  return (
+    <div>
+      <section id='Homepage'>
+        <Navbar />
+        <Hero />
+      </section>
+      <section id='Services'><Parallax type="services" /></section>
+      <section>Services</section>
+      <section id='Portfolio'><Parallax type="portfolio" /></section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id='Contact'>Contact</section>
+    </div>
+  );
 };
 
 export default App;
