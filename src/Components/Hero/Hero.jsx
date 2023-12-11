1c1b66059581b8d4bb0721f6a1caf7de778a749f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-11 16:09:37 +0530

    Update Hero and Navbar styling

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,59 @@
 import { motion } from "framer-motion"
 import './Hero.scss';
+
+const textVariants = {
+  initial:{
+    x:-500,
+    opacity:0,
+  },
+  animate:{
+    x:0,
+    opacity:1,
+    transition:{
+      duration:1,
+      staggerChildren:0.1,
+    },
+  },
+  scrollButton:{
+    opacity:0,
+    y:10,
+    transition:{
+      duration:2,
+      repeat:Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial:{
+    x:0,
+  },
+  animate:{
+    x:"-220%",
+    transition:{
+      repeat:Infinity,
+      duration:20,
+    },
+  },
+};
+
+
 const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="textContainer">
-          <h2>Kirttinath Ojha</h2>
-          <h1>Web Developer & UI Designer</h1>
-          <div className="buttons">
-            <button>See the latest Works</button>
-            <button>Contact Me</button>
-          </div>
-          <img src="/scroll.png" alt="" />
-        </div>
+        <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+          <motion.h2 variants={textVariants}>Kirttinath Ojha</motion.h2>
+          <motion.h1 variants={textVariants}>Web Developer & UI Designer</motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>See the latest Works</motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="" />
+        </motion.div>
       </div>
-
+        <motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
+          WebDeveloper Designer 
+        </motion.div>
       <div className="imageContainer">
         <img src="/me.png" alt="" />
       </div>
