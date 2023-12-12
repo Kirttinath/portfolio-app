0d6ef03863584ce981853d1d9059a1d664b6258c
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-12 15:08:00 +0530

    Added Portfolio component to the App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
 import Parallax from './Components/Parallax/Parallax';
+import Portfolio from './Components/Portfolio/Portfolio';
 import Services from './Components/Services/Services';
 import './app.scss';
 
@@ -13,11 +14,9 @@ const App = () => {
         <Hero />
       </section>
       <section id='Services'><Parallax type="services" /></section>
-      <section><Services/></section>
+      <section><Services /></section>
       <section id='Portfolio'><Parallax type="portfolio" /></section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
+      <Portfolio />
       <section id='Contact'>Contact</section>
     </div>
   );
