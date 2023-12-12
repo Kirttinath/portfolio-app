bdbd88227509586efa3ecc8e11aa79bd99d2cf22
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-12 13:02:42 +0530

    Add text "Know pain, Feel pain and Accept pain. Those who do not know the pain will never understand true Peace" to the Services page.

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,17 @@
 import './Services.scss';
+import { motion } from 'framer-motion';
+
 
 const Services = () => {
   return (
     <div className="services">
-      
+      <motion.div className="textContainer">
+
+        <p>Know pain, Feel pain and Accept pain<br/>Those who do not know the pain will never understand true Peace</p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer"></motion.div>
+      <motion.div className="listContainer"></motion.div>
     </div>
   )
 }
