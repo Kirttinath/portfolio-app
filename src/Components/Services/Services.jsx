42000cfd86b76e0f091180e66a41725832382096
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-12 15:03:24 +0530

    Added Framer Motion animations to the Services component.

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,27 +1,56 @@
+import { useRef } from 'react';
 import './Services.scss';
-import { motion } from 'framer-motion';
+import { color, motion, useInView } from 'framer-motion';
+
+
+const variants={
+  initial: {
+    x: -500,
+    y:100,
+    opacity:0,
+  },
+  animate:{
+   x:0,
+   opacity: 1,
+   y: 0,
+   transition:{
+    duration: 1,
+    staggerChildren: 0.1,
+   },
+  },
+};
 
 
 const Services = () => {
+
+   const ref = useRef();
+  const isInView = useInView(ref, {margin:"-100px"});
   return (
-    <div className="services">
-      <motion.div className="textContainer">
+    <motion.div 
+    className="services" 
+    variants={variants} 
+    initial="initial" 
+    //whileInView="animate"
+    ref={ref}
+    animate={isInView && "animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
 
         <p>Know pain, Feel pain and Accept pain<br/>Those who do not know the pain will never understand true Peace</p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
             <img src="/people.webp" alt="" />
-            <h1>Unique <b>Ideas</b></h1>
+            <h1>Unique <motion.b whileHover={{color:"orange"}}>Ideas</motion.b></h1>
 
         </div>
         <div className="title">
-            <h1><b>About My </b>Study.</h1>
+            <h1><motion.b whileHover={{color:"orange"}}>About My </motion.b>Study.</h1>
             <button>What I do ?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants={variants}>
         <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
             <h2>Branding</h2>
             <p>Lorem ipsum dolor sit amet. Ut nesciunt molestias in temporibus maxime non ullam consectetur qui voluptate velit eos dolor sunt sit voluptatum ipsa est numquam repudiandae. Vel consectetur consequatur aut velit illum hic nisi earum qui sint sunt et consequatur provident qui accusamus autem. Aut tenetur voluptatum et doloribus</p>
@@ -43,7 +72,7 @@ const Services = () => {
             <button>Go</button>
         </motion.div>
       </motion.div>
-    </div>
+    </motion.div>
   )
 }
 
