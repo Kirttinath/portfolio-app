6efd920496196cf4a9bd5dc0184745d9b66187d4
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-12 17:17:15 +0530

    Added the Contact component to the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Contact from './Components/Contact/Contact';
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
 import Parallax from './Components/Parallax/Parallax';
@@ -17,7 +18,9 @@ const App = () => {
       <section><Services /></section>
       <section id='Portfolio'><Parallax type="portfolio" /></section>
       <Portfolio />
-      <section id='Contact'>Contact</section>
+      <section id='Contact'>
+        <Contact/>
+      </section>
     </div>
   );
 };
