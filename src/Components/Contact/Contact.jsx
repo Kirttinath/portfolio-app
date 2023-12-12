201747ab42c2e35653eaf81ce88dc307569a1699
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-12 17:26:26 +0530

    adding apost

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,7 +3,10 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <div className="contact">
-      
+      <div className="textContainer">
+        <h1>Let&apos;s work Together</h1>
+      </div>
+      <div className="formContainer"></div>
     </div>
   )
 }
