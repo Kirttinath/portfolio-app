db7d613329a39a881c9a3d47870421e87336bc01
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-12 17:30:47 +0530

    Add contact details to the Contact page.

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,6 +5,18 @@ const Contact = () => {
     <div className="contact">
       <div className="textContainer">
         <h1>Let&apos;s work Together</h1>
+        <div className="item">
+          <h2>Mail</h2>
+          <span>kirttinathojha000@gmail.com</span>
+        </div>
+        <div className="item">
+          <h2>Address</h2>
+          <span>Sailashree Vihar-6</span>
+        </div>
+        <div className="item">
+          <h2>Phone</h2>
+          <span>+91 8114888187</span>
+        </div>
       </div>
       <div className="formContainer"></div>
     </div>
